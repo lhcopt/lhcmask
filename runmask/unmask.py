afe3e9266a379952bf799f3749176f06faec2d31
giadarol <giovanni.iadarola@cern.ch>
2020-02-11 18:33:15 +0100

    Status

--- runmask/unmask.py
+++ runmask/unmask.py
@@ -1,27 +1,4 @@
 
-def unmask(mask_filename, parameters,
-        output_filename=None):
-
-    with open(mask_filename, 'r') as fid:
-        content = fid.read()
-
-    for kk in parameters.keys():
-        content = content.replace(kk, str(parameters[kk]))
-
-    if output_filename is None:
-        outfname = None
-    elif output_filename == 'auto':
-        outfname = mask_filename+'.unmasked'
-    else:
-        outfname = output_filename
-
-    if outfname is not None:
-        with open(outfname, 'w') as fid:
-            fid.write(content)
-
-    return content
-
-
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Unmasks a madx maskfile.')
