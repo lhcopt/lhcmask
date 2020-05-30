85424bfe4fae0f446ba13981e66e2004de8e4ae8
giadarol <giovanni.iadarola@cern.ch>
2020-05-30 08:19:59 +0200

    Finalized dataframe generation function

--- examples/hl_lhc_collisions_python/003_newbb.py
+++ examples/hl_lhc_collisions_python/003_newbb.py
@@ -151,89 +151,22 @@ if generate_b4_from_b2:
 
 
 # Install beam beam
-
-# Temp (to be redefined)
-ip_names = ['ip1', 'ip2', 'ip5', 'ip8']
-numberOfLRPerIRSide = [25, 20, 25, 20]
-harmonic_number = 35640
-bunch_spacing_buckets = 10
-numberOfHOSlices = 11
-bunch_population_ppb = None #1.2e11
-sigmaz_m = None #0.075
-
-circumference = mad.sequence.lhcb1.beam.circ
-madx_reference_bunch_charge = mad.sequence.lhcb1.beam.npart
-relativistic_gamma = mad.sequence.lhcb1.beam.gamma
-relativistic_beta = np.sqrt(1 - 1.0 / relativistic_gamma ** 2)
-if bunch_population_ppb is not None:
-    bunch_charge_ppb = bunch_population_ppb
-else:
-    bunch_charge_ppb = madx_reference_bunch_charge
-
-if sigmaz_m  is not None:
-    sigt = sigmaz_m
-else:
-    sigt = mad.sequence.lhcb1.beam.sigt
-
-import bb_tools as bbt
-bb_df_b1 = bbt.generate_set_of_bb_encounters_1beam(
-    circumference, harmonic_number,
-    bunch_spacing_buckets,
-    numberOfHOSlices, bunch_charge_ppb, sigt,
-    relativistic_beta, ip_names, numberOfLRPerIRSide,
-    beam_name = 'b1',
-    other_beam_name = 'b2')
-
-bb_df_b2 = bbt.generate_set_of_bb_encounters_1beam(
-    circumference, harmonic_number,
-    bunch_spacing_buckets,
-    numberOfHOSlices, bunch_charge_ppb, sigt,
-    relativistic_beta, ip_names, numberOfLRPerIRSide,
-    beam_name = 'b2',
-    other_beam_name = 'b1')
-
-# Generate mad info
-bbt.generate_mad_bb_info(bb_df_b1, mode='dummy')
-bbt.generate_mad_bb_info(bb_df_b2, mode='dummy')
-
-# Install dummy bb lenses in mad sequences
-bbt.install_lenses_in_sequences(mad,
-    bb_data_frames=[bb_df_b1, bb_df_b2],
-    beam_names=['b1', 'b2'],
-    sequence_names=['lhcb1', 'lhcb2'])
-
-# Use mad survey and twiss to get geometry and locations of all encounters
-bbt.get_geometry_and_optics_b1_b2(mad, bb_df_b1, bb_df_b2)
-
-# Get the position of the IPs in the surveys of the two beams
-ip_position_df = bbt.get_survey_ip_position_b1_b2(mad, ip_names)
-
-# Get geometry and optics at the partner encounter
-bbt.get_partner_corrected_position_and_optics(
-        bb_df_b1, bb_df_b2, ip_position_df)
-
-# Compute separation, crossing plane rotation and crossing angle
-for bb_df in [bb_df_b1, bb_df_b2]:
-    bbt.compute_separations(bb_df)
-    bbt.compute_dpx_dpy(bb_df)
-    bbt.compute_local_crossing_angle_and_plane(bb_df)
-
-# Get bb dataframe and mad model (with dummy bb) for beam 3 and 4
-bb_df_b3 = bbt.get_counter_rotating(bb_df_b1)
-bb_df_b4 = bbt.get_counter_rotating(bb_df_b2)
-bbt.generate_mad_bb_info(bb_df_b3, mode='dummy')
-bbt.generate_mad_bb_info(bb_df_b4, mode='dummy')
-
-# Generate mad info
-bbt.generate_mad_bb_info(bb_df_b1, mode='from_dataframe', madx_reference_bunch_charge=madx_reference_bunch_charge)
-bbt.generate_mad_bb_info(bb_df_b2, mode='from_dataframe', madx_reference_bunch_charge=madx_reference_bunch_charge)
-bbt.generate_mad_bb_info(bb_df_b3, mode='from_dataframe', madx_reference_bunch_charge=madx_reference_bunch_charge)
-bbt.generate_mad_bb_info(bb_df_b4, mode='from_dataframe', madx_reference_bunch_charge=madx_reference_bunch_charge)
+import beambeam as bb
+bb_dfs = bb.generate_bb_dataframes(mad,
+    ip_names=['ip1', 'ip2', 'ip5', 'ip8'],
+    numberOfLRPerIRSide=[25, 20, 25, 20],
+    harmonic_number=35640,
+    bunch_spacing_buckets=10,
+    numberOfHOSlices=11,
+    bunch_population_ppb=None,
+    sigmaz_m=None)
 
 prrrrr
 
 
-mad.call("modules/module_03_beambeam.madx")
+
+
+# mad.call("modules/module_03_beambeam.madx")
 mad.call("modules/module_04_errors.madx")
 mad.call("modules/module_05_tuning.madx")
 mad.call("modules/module_06_generate.madx")
