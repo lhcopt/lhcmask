098480de759b3afcccd1fa1c3d19fc334cd3a4e2
giadarol <giovanni.iadarola@cern.ch>
2020-06-18 09:37:56 +0200

    A check on leveling module

--- examples/hl_lhc_collisions_python/004_pymask.py
+++ examples/hl_lhc_collisions_python/004_pymask.py
@@ -17,9 +17,9 @@ import optics_specific_tools as ost
 # Select mode
 #mode = 'b1_without_bb'
 #mode = 'b1_with_bb'
-#mode = 'b1_with_bb_legacy_macros'
-mode = 'b4_without_bb'
-mode = 'b4_from_b2_without_bb'
+mode = 'b1_with_bb_legacy_macros'
+#mode = 'b4_without_bb'
+#mode = 'b4_from_b2_without_bb'
 #mode = 'b4_from_b2_with_bb'
 
 # Tolarances for checks [ip1, ip2, ip5, ip8]
