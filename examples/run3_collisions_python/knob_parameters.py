2e6a35d3635f0be1a285a0250ea3734a8bc95ace
giadarol <giovanni.iadarola@cern.ch>
2020-06-18 16:20:22 +0200

    Run 3: first twiss and check

---
+++ examples/run3_collisions_python/knob_parameters.py
@@ -0,0 +1,29 @@
+
+knob_parameters = {
+
+    #IP specific orbit settings
+    'par_on_x1'                : 150.,
+    'par_on_x5'                : 150.,
+    'par_on_sep1'              : 0,
+    'par_on_sep5'              : 0,
+    'par_on_sep2h'             : 1,
+    'par_on_sep2v'             : 0,
+    'par_on_x2h'               : 0,
+    'par_on_x2v'               : 200,
+    'par_on_sep8h'             : 0,
+    'par_on_sep8v'             : 1,
+    'par_on_x8h'               : 0,
+    'par_on_x8v'               : 135,
+
+    # Dispersion correction knob
+    'par_on_disp'              : 1,            # Could be optics-dependent
+
+    # Magnets of the experiments
+    'par_on_alice'             : 1,
+    'par_on_lhcb'              : 1,
+
+    'par_on_sol_atlas'         : 0,
+    'par_on_sol_cms'           : 0,
+    'par_on_sol_alice'         : 0,
+
+}
