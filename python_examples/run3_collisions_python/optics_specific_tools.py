266c8fad195d605ebaaa33da9802247a28f334ff
giadarol <giovanni.iadarola@cern.ch>
2020-10-22 12:18:34 +0200

    Status

--- python_examples/run3_collisions_python/optics_specific_tools.py
+++ python_examples/run3_collisions_python/optics_specific_tools.py
@@ -7,50 +7,57 @@ import pymask as pm
 
 def build_sequence(mad, beam):
 
-    # Select beam
+    slicefactor = 2 # For testing
+    # slicefactor = 8 # For production
+
+    pm.make_links(force=True, links_dict={
+        'optics_runII': '/afs/cern.ch/eng/lhc/optics/runII',
+        'optics_runIII': '/afs/cern.ch/eng/lhc/optics/runIII',})
+
+    mylhcbeam = int(beam)
+
+    mad.input('ver_lhc_run = 3')
+
     mad.input(f'mylhcbeam = {beam}')
+    mad.input('option, -echo,warn, -info;')
 
-    # Make link to optics toolkit
-    pm.make_links({'optics_toolkit': '/afs/cern.ch/eng/lhc/optics/HLLHCV1.4/toolkit'},
-        force=True)
+    # optics dependent macros (for splitting)
+    mad.call('optics_runII/2018/toolkit/macro.madx')
 
-    mad.input('''
+    # Redefine macros
+    _redefine_crossing_save_disable_restore(mad)
 
-        ! Specify machine version
-        ver_lhc_run = 0;
-        ver_hllhc_optics = 1.4;
+    # optics independent macros
+    mad.call('tools/optics_indep_macros.madx')
 
-        ! Get the toolkit
-        call, file="/afs/cern.ch/eng/lhc/optics/HLLHCV1.4/toolkit/macro.madx";
+    assert mylhcbeam in [1, 2, 4], "Invalid mylhcbeam (it should be in [1, 2, 4])"
 
+    if mylhcbeam in [1, 2]:
+        mad.call('optics_runII/2018/lhc_as-built.seq')
+    else:
+        mad.call('optics_runII/2018/lhcb4_as-built.seq')
 
-        ! Build sequence
-        option, -echo,-warn,-info;
-        if (mylhcbeam==4){
-          call,file="/afs/cern.ch/eng/lhc/optics/runIII/lhcb4.seq";
-        } else {
-          call,file="/afs/cern.ch/eng/lhc/optics/runIII/lhc.seq";
-        };
-        option, -echo, warn,-info;
+    # New IR7 MQW layout and cabling
+    mad.call('optics_runIII/RunIII_dev/IR7-Run3seqedit.madx')
 
-        !Install HL-LHC
-        call, file="/afs/cern.ch/eng/lhc/optics/HLLHCV1.4/hllhc_sequence.madx";
+    # Makethin part
+    if slicefactor > 0:
+        # the variable in the macro is slicefactor
+        mad.input(f'slicefactor={slicefactor};')
+        mad.call('optics_runII/2018/toolkit/myslice.madx')
+        mad.beam()
+        for my_sequence in ['lhcb1','lhcb2']:
+            if my_sequence in list(mad.sequence):
+                mad.input(f'use, sequence={my_sequence}; makethin,'
+                     'sequence={my_sequence}, style=teapot, makedipedge=true;')
+    else:
+        warnings.warn('The sequences are not thin!')
 
-
-        ! Slice nominal sequence
-        exec, myslice;
-
-        ! Install placeholder elements for errors (set to zero)
-        call, file="errors/HL-LHC/install_MQXF_fringenl.madx";    ! adding fringe place holder
-        call, file="errors/HL-LHC/install_MCBXFAB_errors.madx";   ! adding D1 corrector placeholders in IR1/5 (for errors)
-        call, file="errors/HL-LHC/install_MCBRD_errors.madx";     ! adding D2 corrector placeholders in IR1/5 (for errors)
-
-        !Cycling w.r.t. to IP3 (mandatory to find closed orbit in collision in the presence of errors)
-        if (mylhcbeam<3){
-          seqedit, sequence=lhcb1; flatten; cycle, start=IP3; flatten; endedit;
-        };
-        seqedit, sequence=lhcb2; flatten; cycle, start=IP3; flatten; endedit;
-        ''')
+    # Cycling w.r.t. to IP3 (mandatory to find closed orbit in collision in the presence of errors)
+    for my_sequence in ['lhcb1','lhcb2']:
+        if my_sequence in list(mad.sequence):
+            mad.input(f'seqedit, sequence={my_sequence}; flatten;'
+                        'cycle, start=IP3; flatten; endedit;')
 
 def apply_optics(mad, optics_file):
     mad.call(optics_file)
@@ -113,6 +120,9 @@ def twiss_and_check(mad, sequences_to_check, twiss_fname,
 
     return twiss_dfs, other_data
 
+
+ARRIVATI QUA
+
 def lumi_control(mad, twiss_dfs, python_parameters, mask_parameters, knob_names):
     from scipy.optimize import least_squares
 
@@ -141,6 +151,63 @@ def lumi_control(mad, twiss_dfs, python_parameters, mask_parameters, knob_names)
             * mad.sequence.lhcb1.beam['e'+sep_plane_ip2])
     mad.globals[sep_knobname_ip2] = (np.sign(mad.globals[sep_knobname_ip2])
             * mask_parameters['par_fullsep_in_sigmas_ip2']*sigma_sep_b1_ip2/2*1e3)
+
+
+def _redefine_crossing_save_disable_restore(mad):
+
+    mad.input('''
+    crossing_save: macro = {
+    on_x1_aux=on_x1;on_sep1_aux=on_sep1;on_a1_aux=on_a1;on_o1_aux=on_o1;
+    on_x2_aux=on_x2;on_sep2_aux=on_sep2;on_a2_aux=on_a2;on_o2_aux=on_o2; on_oe2_aux=on_oe2;
+    on_x5_aux=on_x5;on_sep5_aux=on_sep5;on_a5_aux=on_a5;on_o5_aux=on_o5;
+    on_x8_aux=on_x8;on_sep8_aux=on_sep8;on_a8_aux=on_a8;on_o8_aux=on_o8;
+    on_x2h_aux=on_x2h;
+    on_x2v_aux=on_x2v;
+    on_sep2h_aux=on_sep2h;
+    on_sep2v_aux=on_sep2v;
+    on_x8h_aux=on_x8h;
+    on_x8v_aux=on_x8v;
+    on_sep8h_aux=on_sep8h;
+    on_sep8v_aux=on_sep8v;
+    on_disp_aux=on_disp;
+    on_alice_aux=on_alice;
+    on_lhcb_aux=on_lhcb;
+    };
+
+    crossing_disable: macro={
+    on_x1=0;on_sep1=0;on_a1=0;on_o1=0;
+    on_x2=0;on_sep2=0;on_a2=0;on_o2=0;on_oe2=0;
+    on_x5=0;on_sep5=0;on_a5=0;on_o5=0;
+    on_x8=0;on_sep8=0;on_a8=0;on_o8=0;
+    on_x2h=0;
+    on_x2v=0;
+    on_sep2h=0;
+    on_sep2v=0;
+    on_x8h=0;
+    on_x8v=0;
+    on_sep8h=0;
+    on_sep8v=0;
+    on_disp=0;
+    on_alice=0; on_lhcb=0;
+    };
+
+    crossing_restore: macro={
+    on_x1=on_x1_aux;on_sep1=on_sep1_aux;on_a1=on_a1_aux;on_o1=on_o1_aux;
+    on_x2=on_x2_aux;on_sep2=on_sep2_aux;on_a2=on_a2_aux;on_o2=on_o2_aux; on_oe2=on_oe2_aux;
+    on_x5=on_x5_aux;on_sep5=on_sep5_aux;on_a5=on_a5_aux;on_o5=on_o5_aux;
+    on_x8=on_x8_aux;on_sep8=on_sep8_aux;on_a8=on_a8_aux;on_o8=on_o8_aux;
+    on_x2h=on_x2h_aux;
+    on_x2v=on_x2v_aux;
+    on_sep2h=on_sep2h_aux;
+    on_sep2v=on_sep2v_aux;
+    on_x8h=on_x8h_aux;
+    on_x8v=on_x8v_aux;
+    on_sep8h=on_sep8h_aux;
+    on_sep8v=on_sep8v_aux;
+    on_disp=on_disp_aux;
+    on_alice=on_alice_aux; on_lhcb=on_lhcb_aux;
+    };
+    ''')
 
 
 def _check_beta_at_ips_against_madvars(beam, twiss_df, variable_dicts, tol):
