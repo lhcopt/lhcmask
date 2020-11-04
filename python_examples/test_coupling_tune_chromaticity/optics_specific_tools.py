985f8e4527231c43bf9dcdd577463c4463d87131
giadarol <giovanni.iadarola@cern.ch>
2020-11-04 14:41:10 +0100

    Update test coupling (other tests pass correctly)

--- python_examples/test_coupling_tune_chromaticity/optics_specific_tools.py
+++ python_examples/test_coupling_tune_chromaticity/optics_specific_tools.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import pymask as pm
 
 # The parts marked by (*) in following need to be
@@ -53,79 +55,20 @@ def build_sequence(mad, beam):
 def apply_optics(mad, optics_file):
     mad.call(optics_file)
 
-def check_beta_at_ips_against_madvars(beam, twiss_df, variable_dicts, tol):
-    twiss_value_checks=[]
-    for iip, ip in enumerate([1,2,5,8]):
-        for plane in ['x', 'y']:
-            # (*) Adapet based on knob definitions
-            twiss_value_checks.append({
-                    'element_name': f'ip{ip}:1',
-                    'keyword': f'bet{plane}',
-                    'varname': f'bet{plane}ip{ip}b{beam}',
-                    'tol': tol[iip]})
 
-    pm.check_twiss_against_madvars(twiss_value_checks, twiss_df, variable_dicts)
+def set_optics_specific_knobs(mad, knob_settings, mode=None):
 
-
-def set_optics_specific_knobs(mad, knob_parameters, mode=None):
-
-    kp = knob_parameters
-
-    mad.set_variables_from_dict(params=kp)
-
-    # Set IP knobs
-    mad.globals['on_x1'] = kp['par_x1']
-    mad.globals['on_sep1'] = kp['par_sep1']
-    mad.globals['on_x2'] = kp['par_x2']
-    mad.globals['on_sep2'] = kp['par_sep2']
-    mad.globals['on_x5'] = kp['par_x5']
-    mad.globals['on_sep5'] = kp['par_sep5']
-    mad.globals['on_x8'] = kp['par_x8']
-    mad.globals['on_sep8'] = kp['par_sep8']
-    mad.globals['on_a1'] = kp['par_a1']
-    mad.globals['on_o1'] = kp['par_o1']
-    mad.globals['on_a2'] = kp['par_a2']
-    mad.globals['on_o2'] = kp['par_o2']
-    mad.globals['on_a5'] = kp['par_a5']
-    mad.globals['on_o5'] = kp['par_o5']
-    mad.globals['on_a8'] = kp['par_a8']
-    mad.globals['on_o8'] = kp['par_o8']
-    mad.globals['on_crab1'] = kp['par_crab1']
-    mad.globals['on_crab5'] = kp['par_crab5']
-    mad.globals['on_disp'] = kp['par_on_disp']
+    # Copy knob settings to mad variable space
+    mad.set_variables_from_dict(params=knob_settings)
 
     # A check
     if mad.globals.nrj < 500:
-        assert kp['par_on_disp'] == 0
+        assert knob_settings['on_disp'] == 0
 
-    # Spectrometers at experiments
-    if kp['par_on_alice'] == 1:
-        mad.globals.on_alice = 7000./mad.globals.nrj
-    if kp['par_on_lhcb'] == 1:
-        mad.globals.on_lhcb = 7000./mad.globals.nrj
+    # A knob redefinition
+    mad.input('on_alice := on_alice_normalized * 7000./nrj;')
+    mad.input('on_lhcb := on_lhcb_normalized * 7000./nrj;')
 
-    # Solenoids at experiments
-    mad.globals.on_sol_atlas = kp['par_on_sol_atlas']
-    mad.globals.on_sol_cms = kp['par_on_sol_cms']
-    mad.globals.on_sol_alice = kp['par_on_sol_alice']
-
-
-def check_separations_at_ips_against_madvars(twiss_df_b1, twiss_df_b2,
-        variables_dict, tol):
-
-    separations_to_check = []
-    for iip, ip in enumerate([1,2,5,8]):
-        for plane in ['x', 'y']:
-            # (*) Adapet based on knob definitions
-            separations_to_check.append({
-                    'element_name': f'ip{ip}:1',
-                    'scale_factor': -2*1e-3,
-                    'plane': plane,
-                    # knobs like on_sep1h, onsep8v etc
-                    'varname': f'on_sep{ip}'+{'x':'h', 'y':'v'}[plane],
-                    'tol': tol[iip]})
-    pm.check_separations_against_madvars(separations_to_check,
-            twiss_df_b1, twiss_df_b2, variables_dict)
 
 def twiss_and_check(mad, sequences_to_check, twiss_fname,
         tol_beta=1e-3, tol_sep=1e-6, save_twiss_files=True,
@@ -151,7 +94,7 @@ def twiss_and_check(mad, sequences_to_check, twiss_fname,
     if check_betas_at_ips:
         for ss in sequences_to_check:
             tt = twiss_dfs[ss]
-            check_beta_at_ips_against_madvars(beam=ss[-1],
+            _check_beta_at_ips_against_madvars(beam=ss[-1],
                     twiss_df=tt,
                     variable_dicts=var_dict,
                     tol=tol_beta)
@@ -160,7 +103,7 @@ def twiss_and_check(mad, sequences_to_check, twiss_fname,
     if check_separations_at_ips:
         twiss_df_b1 = twiss_dfs['lhcb1']
         twiss_df_b2 = twiss_dfs['lhcb2']
-        check_separations_at_ips_against_madvars(twiss_df_b1, twiss_df_b2,
+        _check_separations_at_ips_against_madvars(twiss_df_b1, twiss_df_b2,
                 var_dict, tol=tol_sep)
         print('IP separation test against knobs passed!')
 
@@ -169,3 +112,63 @@ def twiss_and_check(mad, sequences_to_check, twiss_fname,
     other_data['summ_dfs'] = summ_dfs
 
     return twiss_dfs, other_data
+
+def lumi_control(mad, twiss_dfs, configuration, knob_names):
+    from scipy.optimize import least_squares
+
+    # Leveling in IP8
+    sep_plane_ip8 = configuration['sep_plane_ip8']
+    sep_knobname_ip8 = knob_names['sepknob_ip8_mm']
+
+    L_target_ip8 = configuration['lumi_ip8']
+    def function_to_minimize_ip8(sep8_m):
+        my_dict_IP8=pm.get_luminosity_dict(
+            mad, twiss_dfs, 'ip8', configuration['nco_IP8'])
+        my_dict_IP8[sep_plane_ip8 + '_1']=np.abs(sep8_m)
+        my_dict_IP8[sep_plane_ip8 + '_2']=-np.abs(sep8_m)
+        return np.abs(pm.luminosity(**my_dict_IP8) - L_target_ip8)
+    sigma_sep_b1_ip8=np.sqrt(twiss_dfs['lhcb1'].loc['ip8:1']['bet'+sep_plane_ip8]
+               * mad.sequence.lhcb1.beam['e'+sep_plane_ip8])
+    optres_ip8=least_squares(function_to_minimize_ip8, sigma_sep_b1_ip8)
+    mad.globals[sep_knobname_ip8] = (np.sign(mad.globals[sep_knobname_ip8])
+                                * np.abs(optres_ip8['x'][0])*1e3)
+
+    # Halo collision in IP2
+    sep_plane_ip2 = configuration['sep_plane_ip2']
+    sep_knobname_ip2 = knob_names['sepknob_ip2_mm']
+    sigma_sep_b1_ip2=np.sqrt(
+            twiss_dfs['lhcb1'].loc['ip2:1']['bet'+sep_plane_ip2]
+            * mad.sequence.lhcb1.beam['e'+sep_plane_ip2])
+    mad.globals[sep_knobname_ip2] = (np.sign(mad.globals[sep_knobname_ip2])
+            * configuration['fullsep_in_sigmas_ip2']*sigma_sep_b1_ip2/2*1e3)
+
+
+def _check_beta_at_ips_against_madvars(beam, twiss_df, variable_dicts, tol):
+    twiss_value_checks=[]
+    for iip, ip in enumerate([1,2,5,8]):
+        for plane in ['x', 'y']:
+            # (*) Adapet based on knob definitions
+            twiss_value_checks.append({
+                    'element_name': f'ip{ip}:1',
+                    'keyword': f'bet{plane}',
+                    'varname': f'bet{plane}ip{ip}b{beam}',
+                    'tol': tol[iip]})
+
+    pm.check_twiss_against_madvars(twiss_value_checks, twiss_df, variable_dicts)
+
+def _check_separations_at_ips_against_madvars(twiss_df_b1, twiss_df_b2,
+        variables_dict, tol):
+
+    separations_to_check = []
+    for iip, ip in enumerate([1,2,5,8]):
+        for plane in ['x', 'y']:
+            # (*) Adapet based on knob definitions
+            separations_to_check.append({
+                    'element_name': f'ip{ip}:1',
+                    'scale_factor': -2*1e-3,
+                    'plane': plane,
+                    # knobs like on_sep1h, onsep8v etc
+                    'varname': f'on_sep{ip}'+{'x':'h', 'y':'v'}[plane],
+                    'tol': tol[iip]})
+    pm.check_separations_against_madvars(separations_to_check,
+            twiss_df_b1, twiss_df_b2, variables_dict)
