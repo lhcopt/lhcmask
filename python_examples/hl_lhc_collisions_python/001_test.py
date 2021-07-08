ce92b605073ba17dd0d0b212b6c91b919df672c5
Guido Sterbini <sterbini@hpc-201-11-01-a.cr.cnaf.infn.it>
2021-07-08 17:13:46 +0200

    Adding the deltas of the BB elements

--- python_examples/hl_lhc_collisions_python/001_test.py
+++ python_examples/hl_lhc_collisions_python/001_test.py
@@ -15,7 +15,7 @@ mad.seterr(table="errtab")
 mad.set(format=".15g")
 mad.twiss(rmatrix = True)
 
-
+separation_given_wrt_closed_orbit_4D = True
 tw = mad.table['twiss']
 
 seq_name = 'lhcb1'
@@ -92,11 +92,17 @@ for ii, ee in enumerate(tracker.line.elements):
           px_0 = temp_particles.px[0]
           py_0 = temp_particles.py[0]
           ee.q0 = ee._temp_q0
+
+          if separation_given_wrt_closed_orbit_4D:
+              ee.mean_x += temp_particles.x[0]
+              ee.mean_y += temp_particles.y[0]
+              line.elements[ii].x_bb = ee.mean_x
+              line.elements[ii].y_bb = ee.mean_y
+
           ee.track(temp_particles)
 
           ee.d_px = temp_particles.px - px_0   
           ee.d_py = temp_particles.py - py_0   
-       
           line.elements[ii].d_px = ee.d_px
           line.elements[ii].d_py = ee.d_py
 
@@ -104,13 +110,49 @@ for ii, ee in enumerate(tracker.line.elements):
           temp_particles.py -= ee.d_py  
 
     elif ee.__class__.__name__ == 'BeamBeamBiGaussian3D':
-          pass
+        ee.x_CO = temp_particles.x[0]
+        ee.px_CO = temp_particles.px[0]
+        ee.y_CO = temp_particles.y[0]
+        ee.py_CO = temp_particles.py[0]
+        ee.sigma_CO = temp_particles.zeta[0]
+        ee.delta_CO = temp_particles.delta[0]
+        
+        ee.track(temp_particles)
+
+        ee.Dx_sub = temp_particles.x[0] - ee.x_CO
+        ee.Dpx_sub = temp_particles.px[0] - ee.px_CO
+        ee.Dy_sub = temp_particles.y[0] - ee.y_CO
+        ee.Dpy_sub = temp_particles.py[0] - ee.py_CO
+        ee.Dsigma_sub = temp_particles.zeta[0] - ee.sigma_CO
+        ee.Ddelta_sub = temp_particles.delta[0] - ee.delta_CO
+ 
+        temp_particles.x[0] = ee.x_CO 
+        temp_particles.px[0] = ee.px_CO
+        temp_particles.y[0] = ee.y_CO
+        temp_particles.py[0] = ee.py_CO
+        temp_particles.zeta[0] = ee.sigma_CO
+        temp_particles.delta[0] = ee.delta_CO
+
+        line.elements[ii].x_co = ee.x_CO 
+        line.elements[ii].px_co = ee.px_CO
+        line.elements[ii].y_co = ee.y_CO
+        line.elements[ii].py_co = ee.py_CO
+        line.elements[ii].zeta_co = ee.sigma_CO
+        line.elements[ii].delta_co = ee.delta_CO
+
+        line.elements[ii].d_x = ee.Dx_sub 
+        line.elements[ii].d_px = ee.Dpx_sub
+        line.elements[ii].d_y = ee.Dy_sub
+        line.elements[ii].d_py = ee.Dpy_sub
+        line.elements[ii].d_zeta = ee.Dsigma_sub
+        line.elements[ii].d_delta = ee.Ddelta_sub
+	
+	
     else:
         ee.track(temp_particles)    
 
 
-
-
+dict_line_xtrack = line.to_dict()
 
 
 
@@ -133,3 +175,11 @@ for _ in range(10):
     print(particles.at_turn[0], particles.x[0], particles.y[0],
           particles.zeta[0])
     tracker.track(particles)
+
+tracker_line_xtrack = xt.Tracker(sequence=xl.Line.from_dict(dict_line_xtrack)) 
+particles = xt.Particles(**particle_on_co.to_dict())
+print('Test closed orbit')
+for _ in range(10):
+    print(particles.at_turn[0], particles.x[0], particles.y[0],
+          particles.zeta[0])
+    tracker_line_xtrack.track(particles)
