3990573e1e6b7722f51a374f3609628864c08176
sterbini <guido.sterbini@cern.ch>
2022-02-16 10:32:30 +0100

    Updating the intall_miniconda.sh adding the relevan pip install

--- python_examples/hl_lhc_collisions_python/checks_and_doc/install_miniconda.sh
+++ python_examples/hl_lhc_collisions_python/checks_and_doc/install_miniconda.sh
@@ -1,26 +1,18 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b  -p ./miniconda -f
-rm Miniconda3-latest-Linux-x86_64.sh
-source miniconda/bin/activate
-pip install ipython jupyterlab numpy scipy pandas awkward matplotlib pyarrow pyyaml cpymad 
-
-git clone https://gitlab.cern.ch/mihostet/pytrain.git
-pip install -e pytrain
-
-git clone https://github.com/xsuite/xobjects
-pip install -e xobjects
-
-git clone https://github.com/xsuite/xline
-pip install -e xline
-
-git clone https://github.com/xsuite/xtrack
-pip install -e xtrack
-
-git clone https://github.com/xsuite/xfields
-pip install -e xfields
-
-git clone https://github.com/sixtrack/sixtracktools
-pip install -e sixtracktools
-
-git clone https://github.com/PyCOMPLETE/FillingPatterns.git
-pip install -e FillingPatterns
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh         
+bash Miniconda3-latest-Linux-x86_64.sh -b  -p ./miniconda -f                       
+source miniconda/bin/activate                                                      
+pip install ipython jupyterlab numpy scipy pandas awkward matplotlib               
+pip install pyarrow pyyaml pytest                                                 
+pip install cpymad                                                                 
+pip install xsuite                                                                 
+git clone git@github.com:lhcopt/lhcerrors.git                                      
+git clone git@github.com:lhcopt/lhctoolkit.git                                     
+git clone git@github.com:lhcopt/lhcmask.git                                        
+cd lhcmask                                                                         
+git checkout release/v1.3.0                                                        
+pip install -e .                                                                   
+cd ../                                                                             
+git clone https://github.com/PyCOMPLETE/FillingPatterns.git                        
+pip install ./FillingPatterns                                                      
+python -m pip install sixtracktools                                                
+python -m pip install NAFFlib 
