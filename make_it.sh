0c459d12aa82407667d9acc0a66f82c6d55eb032
sterbini <guido.sterbini@cern.ch>
2022-08-17 12:27:12 +0200

    Adapt the t005 script

--- make_it.sh
+++ make_it.sh
@@ -1,12 +1,13 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b  -p ./miniconda -f
 source miniconda/bin/activate
-python -m pip install ipython numpy scipy pandas psutil xsuite
+python -m pip install ipython numpy scipy pandas psutil xsuite matplotlib
 git clone -b release/v0.1.0 git@github.com:xsuite/tree_maker.git
 python -m pip install -e tree_maker
 git clone git@github.com:lhcopt/lhcmask.git -b release/v1.3.3
 python -m pip install -e lhcmask
 python -m pip install sixtracktools
+python -m pip install NAFFlib
 git clone git@github.com:lhcopt/lhcerrors.git
 git clone git@github.com:lhcopt/lhctoolkit.git
 git clone git@github.com:lhcopt/hllhc15.git
