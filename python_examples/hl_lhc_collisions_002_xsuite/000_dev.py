fedb26aab4d16ba2e8c7463de823768cea3213d5
giadarol <giovanni.iadarola@cern.ch>
2022-10-03 00:07:25 +0200

    Status

--- python_examples/hl_lhc_collisions_002_xsuite/000_dev.py
+++ python_examples/hl_lhc_collisions_002_xsuite/000_dev.py
@@ -19,3 +19,25 @@ tracker_b4 = line_b4.build_tracker()
 
 tw_b1 = tracker_b1.twiss()
 tw_b4 = tracker_b4.twiss()
+
+import pandas as pd
+dfb2 = pd.read_parquet('../hl_lhc_collisions_001_b4_no_bb/twiss_b2_for_b4check_seq_lhcb2.parquet')
+
+tw_b2 = tw_b4.mirror()
+
+
+# tracker_b1.vars['on_x1'] = 0.0
+# tracker_b1.vars['on_x2'] = 0.0
+# tracker_b1.vars['on_x5'] = 0.0
+# tracker_b1.vars['on_x8'] = 0.0
+# tracker_b1.vars['on_sep1'] = 0.0
+# tracker_b1.vars['on_sep2'] = 0.0
+# tracker_b1.vars['on_sep5'] = 0.0
+# tracker_b1.vars['on_sep8'] = 0.0
+# tracker_b1.vars['on_disp'] = 0.0
+# tracker_b1.vars['on_alice'] = 0
+#tracker_b1.vars['on_lhcb'] = 0 #  This one seems not to work
+
+#twflat = tracker_b1.twiss()
+
+
