6b96226de407594b6624c49cfb4759f30ba0b3cd
giadarol <giovanni.iadarola@cern.ch>
2022-10-04 21:04:36 +0200

    Same for b4

--- python_examples/hl_lhc_collisions_001_b4_no_bb/000_pymask.py
+++ python_examples/hl_lhc_collisions_001_b4_no_bb/000_pymask.py
@@ -449,22 +449,22 @@ for ss in '12 23 34 45 56 67 78 81'.split():
    mad_track.input(f'kod.a{ss}{beam_str} = kmax_mo*({i_oct})/imax_mo/({brho});')
 
 
-# # Correct linear coupling
-# qx_fractional, qx_integer = np.modf(configuration['qx0'])
-# qy_fractional, qy_integer = np.modf(configuration['qy0'])
-# coupl_corr_info = pm.coupling_correction(mad_track,
-#         n_iterations=configuration['N_iter_coupling'],
-#         qx_integer=qx_integer, qy_integer=qy_integer,
-#         qx_fractional=qx_fractional, qy_fractional=qy_fractional,
-#         tune_knob1_name=knob_names['qknob_1'][sequence_to_track],
-#         tune_knob2_name=knob_names['qknob_2'][sequence_to_track],
-#         cmr_knob_name=knob_names['cmrknob'][sequence_to_track],
-#         cmi_knob_name=knob_names['cmiknob'][sequence_to_track],
-#         sequence_name=sequence_to_track, skip_use=True)
-# 
-# # Add custom values to coupling knobs
-# mad_track.globals[knob_names['cmrknob'][sequence_to_track]] += configuration['delta_cmr']
-# mad_track.globals[knob_names['cmiknob'][sequence_to_track]] += configuration['delta_cmi']
+# Correct linear coupling
+qx_fractional, qx_integer = np.modf(configuration['qx0'])
+qy_fractional, qy_integer = np.modf(configuration['qy0'])
+coupl_corr_info = pm.coupling_correction(mad_track,
+        n_iterations=configuration['N_iter_coupling'],
+        qx_integer=qx_integer, qy_integer=qy_integer,
+        qx_fractional=qx_fractional, qy_fractional=qy_fractional,
+        tune_knob1_name=knob_names['qknob_1'][sequence_to_track],
+        tune_knob2_name=knob_names['qknob_2'][sequence_to_track],
+        cmr_knob_name=knob_names['cmrknob'][sequence_to_track],
+        cmi_knob_name=knob_names['cmiknob'][sequence_to_track],
+        sequence_name=sequence_to_track, skip_use=True)
+
+# Add custom values to coupling knobs
+mad_track.globals[knob_names['cmrknob'][sequence_to_track]] += configuration['delta_cmr']
+mad_track.globals[knob_names['cmiknob'][sequence_to_track]] += configuration['delta_cmi']
 
 # Check strength limits
 if enable_imperfections:
