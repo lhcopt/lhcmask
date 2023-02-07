2ec9a64f8e0fc9672c41f4ac799ba084618a3e70
giadarol <giovanni.iadarola@cern.ch>
2023-02-07 10:27:36 +0100

    Preparing for test

---
+++ python_examples/mxmask_hllhc14_collision/002_pymask_minimal.py
@@ -0,0 +1,135 @@
+import os
+import json
+import yaml
+import numpy as np
+import pymask as pm
+import xobjects as xo
+import xtrack as xt
+
+from pymask.line_preparation import rename_coupling_knobs_and_coefficients
+from pymask.line_preparation import define_octupole_current_knobs
+from pymask.line_preparation import add_correction_term_to_dipole_correctors
+
+# Import user-defined optics-specific tools
+import optics_specific_tools as ost
+
+# Read config file
+with open('config.yaml','r') as fid:
+    configuration = yaml.safe_load(fid)
+
+# Make links
+if configuration['links']['tracking_tools'] == 'auto':
+    configuration['links']['tracking_tools'] = str(pm._pkg_root.parent.parent.absolute())
+
+for kk in configuration['links'].keys():
+    os.system(f'rm {kk}')
+    os.symlink(os.path.abspath(configuration['links'][kk]), kk)
+
+# Create empty temp folder
+os.system('rm -r temp; mkdir temp')
+
+# Start mad
+Madx = pm.Madxp
+mad = Madx(command_log="mad_collider.log")
+mad.globals.par_verbose = int(configuration['verbose_mad_parts'])
+
+# Build sequence (also creates link to optics_toolkit and calls it)
+ost.build_sequence(mad, beam=1, configuration=configuration)
+
+# Set twiss formats for MAD-X parts (macro from opt. toolkit)
+mad.input('exec, twiss_opt;')
+
+# Apply optics
+ost.apply_optics(mad, optics_file=configuration['optics_file'])
+
+# Attach beam to sequences
+pm.attach_beam_to_sequences(mad, configuration=configuration)
+
+# Set cavity phases
+mad.globals['lagrf400.b1'] = 0.5
+mad.globals['lagrf400.b2'] = 0.
+
+mad.use('lhcb1')
+mad.twiss()
+mad.use('lhcb2')
+mad.twiss()
+
+# Generate beam 4
+mad_b4 = Madx(command_log="mad_b4.log")
+ost.build_sequence(mad_b4, beam=4, configuration=configuration)
+pm.configure_b4_from_b2(mad_b4, mad)
+
+lines_co_ref = {}
+lines_co_ref['lhcb1_co_ref'] = xt.Line.from_madx_sequence(mad.sequence.lhcb1,
+    deferred_expressions=True,
+    expressions_for_element_types=('kicker', 'hkicker', 'vkicker'))
+lines_co_ref['lhcb2_co_ref'] = xt.Line.from_madx_sequence(mad_b4.sequence.lhcb2,
+    deferred_expressions=True,
+    expressions_for_element_types=('kicker', 'hkicker', 'vkicker'))
+
+# Set IP1-IP5 phase and store corresponding reference
+mad.input("call, file='modules/submodule_01c_phase.madx';")
+
+# Update beam 4
+pm.configure_b4_from_b2(mad_b4, mad)
+
+lines_to_track = {}
+for sequence_to_track, mad_track in zip(['lhcb1', 'lhcb2'], [mad, mad_b4]):
+
+    # Install crab cavities
+    if configuration['enable_crabs']:
+        mad_track.input("call, file='optics_toolkit/enable_crabcavities.madx';")
+        # They are left off, they will be swiched on at the end
+        mad_track.globals.on_crab1 = 0
+        mad_track.globals.on_crab5 = 0
+
+    # Save references for tuning and corrections (does crossing restore, restores on_disp)
+    mad_track.input("call, file='modules/submodule_04_1b_save_references.madx';")
+
+    # Force on_disp = 0
+    mad_track.globals.on_disp = 0. # will be restored later
+
+    # Final use --> disable use
+    mad_track.use(sequence_to_track)
+
+    # Install and correct errors
+    if configuration['enable_imperfections']:
+        mad_track.set_variables_from_dict(
+                configuration['pars_for_imperfections'])
+        mad_track.input("call, file='modules/module_04_errors.madx';")
+    else:
+        # Synthesize knobs
+        mad_track.input('call, file="modules/submodule_04a_s1_prepare_nom_twiss_table.madx";')
+        if configuration['enable_knob_synthesis']:
+            mad_track.input('exec, crossing_disable;')
+            mad_track.input("call, file='modules/submodule_04e_s1_synthesize_knobs.madx';")
+        mad_track.input('exec, crossing_restore;')
+
+
+    # Prepare xsuite line
+    line = xt.Line.from_madx_sequence(
+        mad.sequence[sequence_to_track], apply_madx_errors=True,
+        deferred_expressions=True)
+    rename_coupling_knobs_and_coefficients(line=line,
+                                           beamn=int(sequence_to_track[-1]))
+    define_octupole_current_knobs(line=line, beamn=int(sequence_to_track[-1]))
+    lines_to_track[sequence_to_track] = line
+
+
+collider = xt.Multiline(
+    lines={
+        'lhcb1': lines_to_track['lhcb1'],
+        'lhcb2': lines_to_track['lhcb2'],
+        'lhcb1_co_ref': lines_co_ref['lhcb1_co_ref'],
+        'lhcb2_co_ref': lines_co_ref['lhcb2_co_ref'],
+    })
+
+collider['lhcb1_co_ref'].particle_ref = collider['lhcb1'].particle_ref.copy()
+collider['lhcb2_co_ref'].particle_ref = collider['lhcb2'].particle_ref.copy()
+
+add_correction_term_to_dipole_correctors(collider)
+
+# Save the two lines to json
+with open('collider.json', 'w') as fid:
+    dct = collider.to_dict()
+    json.dump(dct, fid, cls=xo.JEncoder)
---
+++ python_examples/mxmask_hllhc14_collision/optics_specific_tools.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+import pymask as pm
+
+# The parts marked by (*) in following need to be
+# adapted according to knob definitions
+
+def build_sequence(mad, beam, configuration):
+
+    optics_version = configuration['optics_version']
+
+    # Select beam
+    mad.input(f'mylhcbeam = {beam}')
+
+    # Make link to optics toolkit
+    pm.make_links({'optics_toolkit':
+                f'optics_repository/toolkit'},
+                force=True)
+
+    mad.input('''
+
+        ! Specify machine version
+        ver_lhc_run = 0;
+        '''
+        f'''ver_hllhc_optics = {optics_version};'''
+        f'''
+        ! Get the toolkit
+        call,file=
+          "optics_repository/toolkit/macro.madx";
+        '''
+        '''
+        ! Build sequence
+        option, -echo,-warn,-info;
+        if (mylhcbeam==4){
+          call,file="optics_repository/../runIII/lhcb4.seq";
+        } else {
+          call,file="optics_repository/../runIII/lhc.seq";
+        };
+        option, -echo, warn,-info;
+        '''
+        f'''
+        !Install HL-LHC
+        call, file=
+          "optics_repository/hllhc_sequence.madx";
+        '''
+        '''
+        ! Slice nominal sequence
+        exec, myslice;
+
+        ! Install placeholder elements for errors (set to zero)
+        call, file="errors/HL-LHC/install_MQXF_fringenl.madx";    ! adding fringe place holder
+        call, file="errors/HL-LHC/install_MCBXFAB_errors.madx";   ! adding D1 corrector placeholders in IR1/5 (for errors)
+        call, file="errors/HL-LHC/install_MCBRD_errors.madx";     ! adding D2 corrector placeholders in IR1/5 (for errors)
+        call, file="errors/HL-LHC/install_NLC_errors.madx";       ! adding non-linear corrector placeholders in IR1/5 (for errors)
+
+        !Cycling w.r.t. to IP3 (mandatory to find closed orbit in collision in the presence of errors)
+        if (mylhcbeam<3){
+          seqedit, sequence=lhcb1; flatten; cycle, start=IP3; flatten; endedit;
+        };
+        seqedit, sequence=lhcb2; flatten; cycle, start=IP3; flatten; endedit;
+        ''')
+
+def apply_optics(mad, optics_file):
+    mad.call(optics_file)
+
+
+def set_optics_specific_knobs(mad, knob_settings, mode=None):
+
+    # Copy knob settings to mad variable space
+    mad.set_variables_from_dict(params=knob_settings)
+
+    # A check
+    if mad.globals.nrj < 500:
+        assert knob_settings['on_disp'] == 0
+
+    # A knob redefinition
+    mad.input('on_alice := on_alice_normalized * 7000./nrj;')
+    mad.input('on_lhcb := on_lhcb_normalized * 7000./nrj;')
+
